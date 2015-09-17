2b1dbe5ca7474eda288ae1dd44b2b5bd17633eb7
acionescu <adrian.ionescu.consulting@gmail.com>
2015-09-17 14:41:55 +0300

    package rename, license, cleanup

--- src/main/java/net/segoia/netcell/client/NetcellSocketClient.java
+++ src/main/java/net/segoia/netcell/client/NetcellSocketClient.java
@@ -1,11 +1,27 @@
-package ro.zg.netcell.client;
+/**
+ * netcell-client - Client library for Netcell ESB
+ * Copyright (C) 2009  Adrian Cristian Ionescu - https://github.com/acionescu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.segoia.netcell.client;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Map;
 
-import ro.zg.util.net.nio.NioClient;
+import net.segoia.util.net.nio.NioClient;
 
 public abstract class NetcellSocketClient {
     protected NioClient engineClient;
--- src/main/java/net/segoia/netcell/client/ThinClientNetcellDao.java
+++ src/main/java/net/segoia/netcell/client/ThinClientNetcellDao.java
@@ -1,27 +1,28 @@
-/*******************************************************************************
- * Copyright 2011 Adrian Cristian Ionescu
- * 
+/**
+ * netcell-client - Client library for Netcell ESB
+ * Copyright (C) 2009  Adrian Cristian Ionescu - https://github.com/acionescu
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
-package ro.zg.netcell.client;
+ */
+package net.segoia.netcell.client;
 
 import java.io.Serializable;
 import java.util.Map;
 
-import ro.zg.netcell.control.Command;
-import ro.zg.netcell.control.CommandResponse;
-import ro.zg.netcell.control.NetCell;
-import ro.zg.netcell.vo.definitions.EntityDefinitionSummary;
+import net.segoia.netcell.control.Command;
+import net.segoia.netcell.control.CommandResponse;
+import net.segoia.netcell.control.NetCell;
+import net.segoia.netcell.vo.definitions.EntityDefinitionSummary;
 
 public class ThinClientNetcellDao implements Serializable {
 
